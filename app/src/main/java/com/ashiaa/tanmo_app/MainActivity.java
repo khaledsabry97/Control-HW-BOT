01f5974a1581a237680a5f1c85749b3e750e9057
Muhammad <Muhammad.Rabieh@gmail.com>
2019-08-03 13:12:44 +0200

    v-0.004 two buttons get http log messages Signed-off-by: Muhammad <Muhammad.Rabieh@gmail.com>

--- app/src/main/java/com/ashiaa/tanmo_app/MainActivity.java
+++ app/src/main/java/com/ashiaa/tanmo_app/MainActivity.java
@@ -3,15 +3,31 @@ package com.ashiaa.tanmo_app;
 import androidx.appcompat.app.AppCompatActivity;
 
 
+import android.content.Context;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-import android.widget.Toast;
+import android.widget.TextView;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONObject;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    private Context context;
 
 
 
-public class MainActivity extends AppCompatActivity
-        implements View.OnClickListener{
+
 
 
     @Override
@@ -21,47 +37,49 @@ public class MainActivity extends AppCompatActivity
 
 
 
-        Button onButton = findViewById(R.id.button_on);
-        Button offButton = findViewById(R.id.button_off);
+        final Button buttonParse = findViewById(R.id.button_parse);
 
-        onButton.setOnClickListener(this);
-        offButton.setOnClickListener(this);
+        /*
+        buttonParse.setOnClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View v) {
+               jsonParse();
+           }
+        });
+        */
     }
 
 
+    public void jsonParse(View v) {
 
-    @Override
-    public void onClick(View v)
-    {
+        final TextView mTextViewResult = (TextView) findViewById(R.id.text_view_result);
+
+        // Instantiate the RequestQueue.
+        RequestQueue queue = Volley.newRequestQueue(this);
+
+        String url = "https://httpbin.org/get";
+
+        // Request a string response from the provided URL.
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        // Display the first 500 characters of the response string.
+                        mTextViewResult.setText("Response is: "+ response.substring(0,500));
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                mTextViewResult.setText("That didn't work!");
+            }
+        });
+
+        // Add the request to the RequestQueue.
+        queue.add(stringRequest);
 
 
 
-        switch (v.getId()) {
-
-            //ON block
-            //-------------------------------------------------------------------------------------
-            case R.id.button_on:
-                Toast.makeText(this, "Button 1 clicked", Toast.LENGTH_SHORT).show();
-
-                //-------------------------------------------------------------------------------------
-
-
-                break;
-
-            //OFF block
-            //-------------------------------------------------------------------------------------
-            case R.id.button_off:
-                Toast.makeText(this, "Button 2 clicked", Toast.LENGTH_SHORT).show();
-
-                //-------------------------------------------------------------------------------------
-
-                break;
-        }
     }
-
-
-
-
 
 
 }
