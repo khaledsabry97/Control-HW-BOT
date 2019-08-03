2f94cbf7ca62c157474d813b221bc6b958a34fbc
Muhammad <Muhammad.Rabieh@gmail.com>
2019-08-03 13:10:26 +0200

    v-0.003 simple two buttons only Signed-off-by: Muhammad <Muhammad.Rabieh@gmail.com>

--- app/src/main/java/com/ashiaa/tanmo_app/MainActivity.java
+++ app/src/main/java/com/ashiaa/tanmo_app/MainActivity.java
@@ -2,70 +2,66 @@ package com.ashiaa.tanmo_app;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.TextView;
-
-import com.android.volley.Request;
-import com.android.volley.RequestQueue;
-import com.android.volley.Response;
-import com.android.volley.VolleyError;
-import com.android.volley.toolbox.StringRequest;
-import com.android.volley.toolbox.Volley;
+import android.widget.Toast;
 
 
-public class MainActivity extends AppCompatActivity {
+
+public class MainActivity extends AppCompatActivity
+        implements View.OnClickListener{
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
+
+        Button onButton = findViewById(R.id.button_on);
+        Button offButton = findViewById(R.id.button_off);
+
+        onButton.setOnClickListener(this);
+        offButton.setOnClickListener(this);
     }
 
 
-    public void onButton(View v ) {
+
+    @Override
+    public void onClick(View v)
+    {
 
 
-        TextView textview = (TextView) findViewById(R.id.editText);
-        textview.setVisibility(View.VISIBLE);
 
-        final TextView textView = (TextView) findViewById(R.id.editText);
-        // ...
+        switch (v.getId()) {
 
-        // Instantiate the RequestQueue.
-        RequestQueue queue = Volley.newRequestQueue(this);
-        String url ="http://www.google.com";
+            //ON block
+            //-------------------------------------------------------------------------------------
+            case R.id.button_on:
+                Toast.makeText(this, "Button 1 clicked", Toast.LENGTH_SHORT).show();
 
-        // Request a string response from the provided URL.
-        StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
-                new Response.Listener<String>() {
-                    @Override
-                    public void onResponse(String response) {
-                        // Display the first 500 characters of the response string.
-                        textView.setText("Response is: "+ response.substring(0,500));
-                    }
-                }, new Response.ErrorListener() {
-            @Override
-            public void onErrorResponse(VolleyError error) {
-                textView.setText("That didn't work!");
-            }
-        });
-
-        // Add the request to the RequestQueue.
-        queue.add(stringRequest);
+                //-------------------------------------------------------------------------------------
 
 
+                break;
+
+            //OFF block
+            //-------------------------------------------------------------------------------------
+            case R.id.button_off:
+                Toast.makeText(this, "Button 2 clicked", Toast.LENGTH_SHORT).show();
+
+                //-------------------------------------------------------------------------------------
+
+                break;
+        }
     }
 
 
-    public void offButton(View v ) {
 
-        TextView textview = (TextView) findViewById(R.id.editText);
-        textview.setVisibility(View.INVISIBLE);
 
-    }
 
 
 }
