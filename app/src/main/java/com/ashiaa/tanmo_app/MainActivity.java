82485ed3a2673f5c4f418caf4d9070513f6e2876
Muhammad <Muhammad.Rabieh@gmail.com>
2019-08-03 13:21:39 +0200

    v-0.007 add sound and arabic to buttons Signed-off-by: Muhammad <Muhammad.Rabieh@gmail.com>

--- app/src/main/java/com/ashiaa/tanmo_app/MainActivity.java
+++ app/src/main/java/com/ashiaa/tanmo_app/MainActivity.java
@@ -1,21 +1,14 @@
 package com.ashiaa.tanmo_app;
 
+
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
-
-
-import com.android.volley.Request;
-import com.android.volley.RequestQueue;
-import com.android.volley.Response;
-import com.android.volley.VolleyError;
-import com.android.volley.toolbox.StringRequest;
-import com.android.volley.toolbox.Volley;
-
 
 
 public class MainActivity extends AppCompatActivity {
@@ -28,8 +21,10 @@ public class MainActivity extends AppCompatActivity {
 
         //on button find
         final Button onButton = findViewById(R.id.button_on);
+        final MediaPlayer OnSound = MediaPlayer.create(this,R.raw.on_sound );
         //off button find
         final Button offButton = findViewById(R.id.button_off);
+        final MediaPlayer OffSound = MediaPlayer.create(this,R.raw.off_sound );
 
         //on button listener
         onButton.setOnClickListener(new View.OnClickListener() {
@@ -39,13 +34,15 @@ public class MainActivity extends AppCompatActivity {
                 offButton.setEnabled(true);
                 //do something
 
+                OnSound.start();
+
                 //Toast message
                 Toast.makeText(MainActivity.this, "الرجاء الإنتظار",
                         Toast.LENGTH_LONG).show();
 
-                onButtonFunction(argOn);
+                requestObject.onButtonFunction(argOn);
                 //debugger message
-                Log.d("tagv_click Button 1","Button 1 clicked");
+                Log.d("tagv_click Button 1","On Button clicked");
             }
         });
 
@@ -59,14 +56,16 @@ public class MainActivity extends AppCompatActivity {
                 onButton.setEnabled(true);
                 //do something
 
+                OffSound.start();
+
                 //Toast message
                 Toast.makeText(MainActivity.this, "الرجاء الإنتظار",
                         Toast.LENGTH_LONG).show();
 
-                offButtonFunction(argOff);
+                requestObject.offButtonFunction(argOff);
 
                 //debugger message
-                Log.d("tagv_click Button 2","Button 2 clicked");
+                Log.d("tagv_click Button 2","Off Button clicked");
             }
         });
 
@@ -74,77 +73,9 @@ public class MainActivity extends AppCompatActivity {
 
 
 
+    //create object of http request class
+    requestFunctions requestObject=new requestFunctions(this);
 
-    public void onButtonFunction(View v) {
-
-        // Instantiate the RequestQueue.
-        RequestQueue queue = Volley.newRequestQueue(this);
-
-        //String url = "https://httpbin.org/get";
-        String url = "https://api.thingspeak.com/channels/338402/fields/3.json?results=3";
-
-        // Request a string response from the provided URL.
-        StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
-                new Response.Listener<String>() {
-                    @Override
-                    public void onResponse(String response) {
-                        //Toast message
-                        Toast.makeText(MainActivity.this, "تم فتح الجهاز",
-                                Toast.LENGTH_LONG).show();
-                        // Display the response in debugger
-                        Log.d("tagv_response", "Response is: "+ response);
-
-                    }
-                }, new Response.ErrorListener() {
-            @Override
-            public void onErrorResponse(VolleyError error) {
-                //Toast message
-                Toast.makeText(MainActivity.this, "خطأ في الإتصال بالجهاز",
-                        Toast.LENGTH_LONG).show();
-                //debugger message
-                Log.d("tagv_onErrorResponse", "That didn't work!");
-            }
-        });
-
-        // Add the request to the RequestQueue.
-        queue.add(stringRequest);
-    }
-
-
-    public void offButtonFunction(View v) {
-
-        // Instantiate the RequestQueue.
-        RequestQueue queue = Volley.newRequestQueue(this);
-
-        //String url = "https://httpbin.org/get";
-        String url = "https://api.thingspeak.com/channels/338402/fields/4.json?results=3";
-
-        // Request a string response from the provided URL.
-        StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
-                new Response.Listener<String>() {
-                    @Override
-                    public void onResponse(String response) {
-                        //Toast message
-                        Toast.makeText(MainActivity.this, "تم غلق الجهاز",
-                                Toast.LENGTH_LONG).show();
-                        // Display the response in debugger
-                        Log.d("tagv_response", "Response is: "+ response);
-
-                    }
-                }, new Response.ErrorListener() {
-            @Override
-            public void onErrorResponse(VolleyError error) {
-                //Toast message
-                Toast.makeText(MainActivity.this, "خطأ في الإتصال بالجهاز",
-                        Toast.LENGTH_LONG).show();
-                //debugger message
-                Log.d("tagv_onErrorResponse", "That didn't work!");
-            }
-        });
-
-        // Add the request to the RequestQueue.
-        queue.add(stringRequest);
-    }
 
 
 
