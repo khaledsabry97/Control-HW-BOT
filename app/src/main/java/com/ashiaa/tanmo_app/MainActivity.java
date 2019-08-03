20bb0a393cc603af3d3053a827b3d06947628b13
Muhammad <Muhammad.Rabieh@gmail.com>
2019-08-03 13:16:17 +0200

    v-0.005 change listener style Signed-off-by: Muhammad <Muhammad.Rabieh@gmail.com>

--- app/src/main/java/com/ashiaa/tanmo_app/MainActivity.java
+++ app/src/main/java/com/ashiaa/tanmo_app/MainActivity.java
@@ -2,84 +2,91 @@ package com.ashiaa.tanmo_app;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-
-import android.content.Context;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-import android.widget.TextView;
+
 
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
-import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
-
-import org.json.JSONObject;
 
 
 public class MainActivity extends AppCompatActivity {
 
-    private Context context;
-
-
-
-
-
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {        //onCreate start
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //on button find
+        final Button onButton = findViewById(R.id.button_on);
+        //off button find
+        final Button offButton = findViewById(R.id.button_off);
 
+        //on button listener
+        onButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View arg0) {
+                onButton.setEnabled(false);
+                offButton.setEnabled(true);
+                //do something
 
-        final Button buttonParse = findViewById(R.id.button_parse);
-
-        /*
-        buttonParse.setOnClickListener(new View.OnClickListener() {
-           @Override
-           public void onClick(View v) {
-               jsonParse();
-           }
+                onButtonFunction(arg0);
+                //debugger message
+                Log.d("tagv_click Button 1","Button 1 clicked");
+            }
         });
-        */
-    }
 
 
-    public void jsonParse(View v) {
+        //off button listener
 
-        final TextView mTextViewResult = (TextView) findViewById(R.id.text_view_result);
+        offButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View arg0) {
+                offButton.setEnabled(false);
+                onButton.setEnabled(true);
+                //do something
+                //debugger message
+                Log.d("tagv_click Button 2","Button 2 clicked");
+            }
+        });
+
+    }                                                           //onCreate end
+
+
+
+
+    public void onButtonFunction(View v) {
 
         // Instantiate the RequestQueue.
         RequestQueue queue = Volley.newRequestQueue(this);
 
-        String url = "https://httpbin.org/get";
+        String url = "https://httpbin.org/json";
 
         // Request a string response from the provided URL.
         StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
                 new Response.Listener<String>() {
                     @Override
                     public void onResponse(String response) {
-                        // Display the first 500 characters of the response string.
-                        mTextViewResult.setText("Response is: "+ response.substring(0,500));
+                        // Display the response in debugger
+                        Log.d("tagv_response", "Response is: "+ response);
                     }
                 }, new Response.ErrorListener() {
             @Override
             public void onErrorResponse(VolleyError error) {
-                mTextViewResult.setText("That didn't work!");
+                //debugger message
+                Log.d("tagv_onErrorResponse", "That didn't work!");
             }
         });
 
         // Add the request to the RequestQueue.
         queue.add(stringRequest);
-
-
-
     }
 
-
-}
+}       //MainActivity end
