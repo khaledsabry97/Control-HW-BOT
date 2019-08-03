a92c292573afb32a3df264c9f1eb60650dd365c6
Muhammad <Muhammad.Rabieh@gmail.com>
2019-08-03 12:58:27 +0200

    v-0.002 - on off simple http request Signed-off-by: Muhammad <Muhammad.Rabieh@gmail.com>

--- app/src/main/java/com/ashiaa/tanmo_app/MainActivity.java
+++ app/src/main/java/com/ashiaa/tanmo_app/MainActivity.java
@@ -7,6 +7,14 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -19,15 +27,42 @@ public class MainActivity extends AppCompatActivity {
 
     public void onButton(View v ) {
 
-        TextView textview = (TextView) findViewById(R.id.textView1);
+
+        TextView textview = (TextView) findViewById(R.id.editText);
         textview.setVisibility(View.VISIBLE);
+
+        final TextView textView = (TextView) findViewById(R.id.editText);
+        // ...
+
+        // Instantiate the RequestQueue.
+        RequestQueue queue = Volley.newRequestQueue(this);
+        String url ="http://www.google.com";
+
+        // Request a string response from the provided URL.
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        // Display the first 500 characters of the response string.
+                        textView.setText("Response is: "+ response.substring(0,500));
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                textView.setText("That didn't work!");
+            }
+        });
+
+        // Add the request to the RequestQueue.
+        queue.add(stringRequest);
+
 
     }
 
 
     public void offButton(View v ) {
 
-        TextView textview = (TextView) findViewById(R.id.textView1);
+        TextView textview = (TextView) findViewById(R.id.editText);
         textview.setVisibility(View.INVISIBLE);
 
     }
