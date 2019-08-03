eb2e3945ed809d2fbbefc3cc01913c21990f2b6c
Muhammad <Muhammad.Rabieh@gmail.com>
2019-08-03 13:23:39 +0200

    v-0.008 try fragment not working Signed-off-by: Muhammad <Muhammad.Rabieh@gmail.com>

--- app/src/main/java/com/ashiaa/tanmo_app/MainActivity.java
+++ app/src/main/java/com/ashiaa/tanmo_app/MainActivity.java
@@ -2,13 +2,8 @@ package com.ashiaa.tanmo_app;
 
 
 import androidx.appcompat.app.AppCompatActivity;
-
-import android.media.MediaPlayer;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
-import android.widget.Button;
-import android.widget.Toast;
+
 
 
 public class MainActivity extends AppCompatActivity {
@@ -19,62 +14,18 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //on button find
-        final Button onButton = findViewById(R.id.button_on);
-        final MediaPlayer OnSound = MediaPlayer.create(this,R.raw.on_sound );
-        //off button find
-        final Button offButton = findViewById(R.id.button_off);
-        final MediaPlayer OffSound = MediaPlayer.create(this,R.raw.off_sound );
 
-        //on button listener
-        onButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View argOn) {
-                onButton.setEnabled(false);
-                offButton.setEnabled(true);
-                //do something
+        buttonFunctions buttonFunctionsObject = new buttonFunctions(this);
 
-                OnSound.start();
-
-                //Toast message
-                Toast.makeText(MainActivity.this, "الرجاء الإنتظار",
-                        Toast.LENGTH_LONG).show();
-
-                requestObject.onButtonFunction(argOn);
-                //debugger message
-                Log.d("tagv_click Button 1","On Button clicked");
-            }
-        });
+        buttonFunctionsObject.OnButtonFunction();
+        buttonFunctionsObject.OffButtonFunction();
 
 
-        //off button listener
-
-        offButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View argOff) {
-                offButton.setEnabled(false);
-                onButton.setEnabled(true);
-                //do something
-
-                OffSound.start();
-
-                //Toast message
-                Toast.makeText(MainActivity.this, "الرجاء الإنتظار",
-                        Toast.LENGTH_LONG).show();
-
-                requestObject.offButtonFunction(argOff);
-
-                //debugger message
-                Log.d("tagv_click Button 2","Off Button clicked");
-            }
-        });
 
     }                                                           //onCreate end
 
 
 
-    //create object of http request class
-    requestFunctions requestObject=new requestFunctions(this);
 
 
 
---
+++ app/src/main/java/com/ashiaa/tanmo_app/buttonFunctions.java
@@ -0,0 +1,126 @@
+package com.ashiaa.tanmo_app;
+
+import android.app.Activity;
+import android.media.MediaPlayer;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class buttonFunctions {
+
+
+
+    public Activity activityAttribute;
+
+    //private final Context context;
+
+    public buttonFunctions(Activity _activity) {
+        this.activityAttribute = _activity;
+        //this.context = context;
+    }
+
+    //on button find
+    final Button onButton = (Button)activityAttribute.findViewById(R.id.button_on);
+
+    //off button find
+    final Button offButton = (Button)activityAttribute.findViewById(R.id.button_off);
+
+    public void OnButtonFunction()
+    {
+
+
+        //on button listener
+        onButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View argOn) {
+                onButton.setEnabled(false);
+                offButton.setEnabled(true);
+                //do something
+
+                onSoundFunction();
+
+                //Toast message
+                Toast.makeText(activityAttribute, "الرجاء الإنتظار",
+                        Toast.LENGTH_LONG).show();
+
+                requestObject.onButtonFunction(argOn);
+                //debugger message
+                Log.d("tagv_click Button 1","On Button clicked");
+            }
+        });
+    }
+
+
+    public void OffButtonFunction()
+    {
+
+
+        //off button listener
+
+        offButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View argOff) {
+                offButton.setEnabled(false);
+                onButton.setEnabled(true);
+                //do something
+
+                offSoundFunction();
+
+                //Toast message
+                Toast.makeText(activityAttribute, "الرجاء الإنتظار",
+                        Toast.LENGTH_LONG).show();
+
+                requestObject.offButtonFunction(argOff);
+
+                //debugger message
+                Log.d("tagv_click Button 2","Off Button clicked");
+            }
+        });
+    }
+
+
+    //on sound function
+    public void onSoundFunction()
+    {
+        final MediaPlayer OnSound = MediaPlayer.create(activityAttribute,R.raw.on_sound );
+
+        OnSound.start();
+
+        OnSound.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+            @Override
+            public void onCompletion(MediaPlayer mp) {
+                // TODO Auto-generated method stub
+                mp.reset();
+                mp.release();
+
+            }
+        });
+
+    }
+
+
+    //on sound function
+    public void offSoundFunction()
+    {
+        final MediaPlayer OffSound = MediaPlayer.create(activityAttribute,R.raw.off_sound );
+
+        OffSound.start();
+
+        OffSound.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+            @Override
+            public void onCompletion(MediaPlayer mp) {
+                // TODO Auto-generated method stub
+                mp.reset();
+                mp.release();
+
+            }
+        });
+
+    }
+
+
+
+    //create object of http request class
+    requestFunctions requestObject=new requestFunctions(this.activityAttribute);
+}
