4800b0a591dcc66def26ce2bccb768898338d649
aSHiAa <aSHiAa.IoT@gmail.com>
2019-09-04 17:55:07 +0200

    review 11 Signed-off-by: aSHiAa <aSHiAa.IoT@gmail.com>

--- app/src/main/java/com/ashiaa/tanmo_app/Model/Constants.java
+++ app/src/main/java/com/ashiaa/tanmo_app/Model/Constants.java
@@ -21,19 +21,22 @@ public class Constants {
     }
     public String getOnUrl()
     {
-        String url = getUrl() +"/on.php";
+        //String url = getUrl() +"/on.php";
+        String url = "http://www.mocky.io/v2/5d6f61ca3100000d0066054f";
         return url;
     }
 
     public String getOffUrl()
     {
-        String url = getUrl() +"/off.php";
+        //String url = getUrl() +"/off.php";
+        String url = "http://www.mocky.io/v2/5d6fc5d431000033a4660901";
         return url;
     }
 
     public String getStatusUrl()
     {
-        String url = getUrl() +"/status.php";
+        //String url = getUrl() +"/status.php";
+        String url = "http://www.mocky.io/v2/5d6fc65c31000037a4660906";
         return url;
     }
 }
